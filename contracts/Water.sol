e477d02acb36be967de0dc0ab410d56bd7de9552
mjawanda1987 <44200959+mjawanda1987@users.noreply.github.com>
2019-12-31 21:46:44 -0700

    Add files via upload

---
+++ contracts/Water.sol
@@ -0,0 +1,235 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Aion {
+    uint256 public serviceFee;
+    function ScheduleCall(uint256 blocknumber, address to, uint256 value, uint256 gaslimit, uint256 gasprice, bytes memory data, bool schedType) public payable returns (uint,address);
+
+}
+
+
+contract Water {
+
+Aion aion;
+
+address private admin;
+
+event getReport (
+    uint pH
+);
+
+struct report {
+    uint ID;
+    uint pH;
+    uint hardness;
+    uint TDS;
+    address sender;
+    bool latest;
+    bool sign;
+}
+
+struct CompanyProp {
+    address CompanyAddress;
+    string Name;
+}
+
+struct RegulatorProp {
+    address RegulatorAddress;
+    string Name;
+}
+
+struct LabProp {
+    address LabAddress;
+    string Name;
+}
+
+mapping (uint => report) public reportStruct ;
+
+mapping (uint => CompanyProp) public Company;
+
+mapping (uint => RegulatorProp) public Regulator;
+
+mapping (uint => LabProp) public Lab;
+
+constructor () public {
+    
+    admin = msg.sender;
+    
+}  
+    
+    modifier OnlyAdmin {
+        require (admin == msg.sender, "Is not an Admin");
+        _;
+    }
+    
+    uint CompanyID;
+       
+    modifier OnlyCompany {
+        for (uint i=0; i < CompanyID; i++){
+            if(Company[i].CompanyAddress == msg.sender){
+            _;
+            return;
+        }
+    }
+        revert();
+}
+
+    uint RegulatorID;
+       
+    modifier OnlyRegulator {
+        for (uint i=0; i < RegulatorID; i++){
+            if(Regulator[i].RegulatorAddress == msg.sender){
+            _;
+            return;
+        }
+    }
+        revert();
+}
+
+    uint LabID;
+       
+    modifier OnlyLab {
+        for (uint i=0; i < LabID; i++){
+            if(Lab[i].LabAddress == msg.sender){
+            _;
+            return;
+        }
+    }
+        revert();
+}
+    
+    function addCompany (address _company, string memory _name) public OnlyAdmin returns (uint) {
+        Company[CompanyID].CompanyAddress = _company;
+        Company[CompanyID].Name = _name;
+        
+        CompanyID += 1;
+        return (CompanyID);
+    }
+    
+    function addRegulator (address _regulator, string memory _name) public OnlyAdmin returns (uint) {
+        Regulator[RegulatorID].RegulatorAddress = _regulator;
+        Regulator[RegulatorID].Name = _name;
+        
+        RegulatorID += 1;
+        return (RegulatorID);
+    }
+    
+    function addLab (address _lab, string memory _name) public OnlyAdmin returns (uint) {
+        Lab[LabID].LabAddress = _lab;
+        Lab[LabID].Name = _name;
+        
+        LabID += 1;
+        return (LabID);
+    }        
+    
+    function getCompany (address _companyAddress) public view returns (string memory cName) {
+        for (uint i=0; i < CompanyID; i++) {
+            
+            if (Company[i].CompanyAddress == _companyAddress){
+            return (Company[i].Name);
+        }
+    }
+}
+    
+    uint reportID;
+    
+    function SubmitReport (uint _pH, uint _hardness, uint _TDS, address _companyAddress) public returns (uint) {
+        
+        //find the latest report and if it is not signed revert state
+        for (uint i=0; i < reportID; i++) {
+            if (reportStruct[i].sender == _companyAddress && reportStruct[i].latest == true && reportStruct[i].sign == false) {
+                revert();
+            }
+        }
+        
+        // find the latest report and change the bool to false before adding a new entry for that 
+        // company
+        for (uint i=0; i < reportID; i++) {
+            if (reportStruct[i].sender == _companyAddress && reportStruct[i].latest == true) {
+                reportStruct[i].latest = false;
+            }
+        }
+        
+        reportStruct[reportID].ID = reportID;
+        reportStruct[reportID].pH = _pH;
+        reportStruct[reportID].hardness = _hardness;
+        reportStruct[reportID].TDS = _TDS;
+        reportStruct[reportID].sender = _companyAddress;
+        reportStruct[reportID].latest = true;
+        reportStruct[reportID].sign = false;
+        
+        reportID += 1;
+        return (reportID);
+    }
+    
+    function scheduleMyfucntion() public {
+        aion = Aion(0xFcFB45679539667f7ed55FA59A15c8Cad73d9a4E);
+        bytes memory data = abi.encodeWithSelector(bytes4(keccak256('myfucntion()')));
+        uint callCost = 200000*1e9 + aion.serviceFee();
+        aion.ScheduleCall.value(callCost)( block.timestamp + 10 minutes, address(this), 0, 200000, 1e9, data, true);
+    }
+    
+
+    function getLastReport () public payable returns (uint ph, uint hardness, uint TDS, bool sign) {
+        for (uint i=0; i < reportID; i++) {
+            if(reportStruct[i].sender == msg.sender && reportStruct[i].latest == true) {
+               return (reportStruct[i].pH, reportStruct[i].hardness, reportStruct[i].TDS, reportStruct[i].sign);
+               //emit getReport(reportStruct[i].pH);             
+            }
+            
+        } 
+        scheduleMyfucntion();    
+    }
+
+    function getReports () public view returns (uint[10] memory pH, uint[10] memory hardness, uint[10] memory TDS, bool[10] memory Latest, bool[10] memory Sign) {
+        uint arrayID;
+        uint[10] memory arraypH;
+        uint[10] memory arrayHardness;
+        uint[10] memory arrayTDS;
+        bool[10] memory arrayLatest;
+        bool[10] memory arraySign;
+        
+        uint i=0;
+        while (i < reportID) {
+            if (reportStruct[i].sender == msg.sender) {
+                arraypH[arrayID] = reportStruct[i].pH;
+                arrayHardness[arrayID] = reportStruct[i].hardness;
+                arrayTDS[arrayID] = reportStruct[i].TDS;
+                arrayLatest[arrayID] = reportStruct[i].latest;
+                arraySign[arrayID] = reportStruct[i].sign;
+                arrayID++;
+            }
+               i++;
+        }
+        
+        return (arraypH, arrayHardness, arrayTDS, arrayLatest, arraySign);
+        
+    }
+    
+    function SignReport () public returns (uint pH, uint hardness, uint TDS, bool latest, bool sign) {
+        for (uint i=0; i < reportID; i++) {
+            if(reportStruct[i].sender == msg.sender && reportStruct[i].latest == true) {
+                reportStruct[i].sign = true;
+               return (reportStruct[i].pH, reportStruct[i].hardness, reportStruct[i].TDS, reportStruct[i].latest,
+               reportStruct[i].sign);
+            }
+    
+        }     
+    
+    }    
+
+    function () external payable {}
+
+
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
